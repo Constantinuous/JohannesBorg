0647c09d961abcf61a8bde30bcd0dfcfbf2a2880
RichardG <richard.gross@maibornwolff.de>
2017-02-04 14:48:19 +0100

    initial commit

---
+++ JohannesBorg.Tests/NUnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace JohannesBorg.Tests
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
