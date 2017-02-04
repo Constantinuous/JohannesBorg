1bc35e87dd1fd96d704157231915f8b53cc5ab8d
RichardG <richard.gross@maibornwolff.de>
2017-02-04 15:49:29 +0100

    added sqlite in-memory database test

--- JohannesBorg.Tests/NUnitTest1.cs
+++ JohannesBorg.Tests/NUnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Data.SQLite;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace JohannesBorg.Tests
@@ -6,10 +7,64 @@ namespace JohannesBorg.Tests
     [TestFixture]
     public class NUnitTest1
     {
+
+
+
         [Test]
         public void TestMethod1()
         {
             new Class1().Should().NotBeNull();
+
+
+            int resultCount = 0;
+            using (SQLiteConnection connection = new SQLiteConnection())
+            {
+                connection.ConnectionString = "FullUri=file::memory:?cache=shared";
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+                    CREATE TABLE Persons
+                    (
+                    PersonID int,
+                    LastName varchar(255),
+                    FirstName varchar(255),
+                    Address varchar(255),
+                    City varchar(255)
+                    ); 
+                ";
+                    command.ExecuteNonQuery();
+                }
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+                        INSERT INTO Persons (PersonID,LastName)
+                            VALUES (1,'blib');
+                    ";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            resultCount++;
+                        }
+                    }
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+                        SELECT * FROM Persons
+                    ";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            resultCount++;
+                        }
+                    }
+                }
+            }
+            resultCount.Should().Be(1);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JohannesBorg
+
+[![Build Status](https://travis-ci.org/Constantinuous/JohannesBorg.svg?branch=master)](https://travis-ci.org/Constantinuous/JohannesBorg)
