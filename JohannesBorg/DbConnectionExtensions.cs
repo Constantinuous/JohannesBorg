b0809690509b0944c9498ef73b5c965bcea24e59
RichardG <richard.gross@maibornwolff.de>
2017-02-04 15:14:03 +0100

    added initial extensions

---
+++ JohannesBorg/DbConnectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+namespace JohannesBorg
+{
+    public static class DbConnectionExtensions
+    {
+        public static void Query<TEntity>(this IDbConnection me)
+        {
+            var test = "test";
+            using (var command = me.CreateCommand())
+            {
+                command.CommandText = $"Select * From {test}";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var value = reader["column"];
+                        var value2 = reader["column1"];
+                    }
+                }
+            }
+        }
+    }
+}
